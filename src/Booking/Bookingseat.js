22b5d9052806335e11fadaf44ea0acacf01d2a98
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-08-17 10:00:49 +0900

    좌석 예매 draft..

--- src/Booking/Bookingseat.js
+++ src/Booking/Bookingseat.js
@@ -1,9 +1,38 @@
-function Bookingseat(){
-    return(
+import button from "bootstrap/js/src/button";
+import {Table} from "react-bootstrap";
+import {useSelector} from "react-redux";
+
+function Bookingseat() {
+
+    let user = useSelector((state) => state.user)
+
+    return (
         <>
-            <div>좌석 예약 페이지</div>
+            <Table striped bordered hover variant="dark">
+                <thead>
+                <tr>{user.name} 님의 예매 현황</tr>
+                <tr>
+                    <th colSpan="4" style={{textAlign: 'center'}}>인원 / 좌석</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td colSpan="2">
+                        좌석 선택
+                    </td>
+                    <td colSpan="2">
+                        영화 이름, 상영관, 날짜, 시간대 뿌리기
+                    </td>
+                </tr>
+                <tr>
+                    <td colSpan="3">좌석 선택 이미지</td>
+                    <td colSpan="1">좌석 설명</td>
+                </tr>
+                </tbody>
+            </Table>
         </>
     );
 }
 
-export default Bookingseat
+
+export default Bookingseat;

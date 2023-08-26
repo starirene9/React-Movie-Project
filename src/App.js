42342239a76d81e6a78e2507185738d721d24c97
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-08-26 13:53:45 +0900

    react- query

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Main from "./MainPage/Main";
 import Detail from "./MainPage/Detail";
 import Booking from "./Booking/Booking";
 import Bookingseat from "./Booking/Bookingseat";
+import axios from "axios";
+import {useQuery} from "react-query";
 
 
 function App() {
@@ -18,11 +20,33 @@ function App() {
     // 복잡한 기호도 시작 기호를 잘 보면 됨
     let navigate = useNavigate();
 
+
+    // useQuery 의 장점 : 성공/실패/로딩 중 쉽게 파악 가능
+    let result = useQuery('작명', () => {
+        return axios.get('https://codingapple1.github.io/userdata.json')
+            .then((a) => {
+                // console.log('요청됨');
+                return a.data
+            }),
+            {staleTime: 2000}
+    // 매번 리프레시 되지 않게 이런 타이머 기능 같은 것도 설정 가능
+        // 실패 했을 때 ajax 요청을 retry 재시도 해 줌
+        // ajax 결과 캐싱 기능이 있음 : 기존 성공 결과를 보여주고 get 요청을 시작함
+
+    })
+
+    // result.data
+    // result.isLoading // 로딩 중일때 true
+    // result.error // 로딩 중일때 false
+
+
     return (
         <>
             <header className="black-nav">
                 <div className="top-nav">
-                    <h1 className="cine-title" onClick={()=>{navigate('/')}}>Together Cinema&nbsp;<span
+                    <h1 className="cine-title" onClick={() => {
+                        navigate('/')
+                    }}>Together Cinema&nbsp;<span
                         style={{color: 'lightgoldenrodyellow', fontSize: 'small'}}> ⭐  b i t n a r a</span></h1>
                     <nav className="top-main-nav1">
                         <ul className="top-main-nav2">
@@ -30,16 +54,35 @@ function App() {
                             <li>회원가입</li>
                             <li>마이페이지</li>
                             <li>고객센터</li>
+                            <li>
+                                {result.isLoading && '로딩 중입니다'}
+                                {result.error && '에러 발생'}
+                                {result.data && result.data.name}
+                            </li>
+                            {/*    result.isLoading && result.data.name 를 써도 똑같음 */}
+                            {/*    {result.isLoading ? '로딩중' : result.data.name}*/}
                         </ul>
                     </nav>
                 </div>
                 <div className="below-nav">
                     <nav>
                         <ul className="below-main-nav">
-                            <li onClick={()=>{navigate('/movie')}}>영화</li>
-                            <li style={{color: 'red', fontWeight: "bold"}} onClick={()=>{navigate('/booking')}}>예매</li>
-                            <li onClick={()=>{navigate('/benefit')}}>혜택</li>
-                            <li onClick={()=>{navigate('/about')}}>Together Cinema</li>
+                            <li onClick={() => {
+                                navigate('/movie')
+                            }}>영화
+                            </li>
+                            <li style={{color: 'red', fontWeight: "bold"}} onClick={() => {
+                                navigate('/booking')
+                            }}>예매
+                            </li>
+                            <li onClick={() => {
+                                navigate('/benefit')
+                            }}>혜택
+                            </li>
+                            <li onClick={() => {
+                                navigate('/about')
+                            }}>Together Cinema
+                            </li>
                         </ul>
                     </nav>
                 </div>
@@ -47,8 +90,8 @@ function App() {
             {/* Routes 들을 Nav 바 밑으로 옮긴다 */}
             {/* element를 컴포넌트화 하면 좋음 */}
             <Routes>
-                <Route path="/" element={<Main movie={data} />}/>
-                <Route path="/movie" element={<Main movie={data} />}/>
+                <Route path="/" element={<Main movie={data}/>}/>
+                <Route path="/movie" element={<Main movie={data}/>}/>
                 <Route path="/movie/:id" element={<Detail movie={data}/>}/>
                 {/*영화의 아이디에 따라 Detail 상세 페이지가 전환되어 나타난다. Useparams 사용하면 Detail 페이지에서 id 사용 가능*/}
                 <Route path="/booking" element={<Booking data={data}/>}/>
@@ -62,7 +105,6 @@ function App() {
     )
         ;
 }
-
 
 
 export default App;
